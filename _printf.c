061059c3bb5ab7f529133d12b98115b40f7021a5
Annonymous0395 <fatoki.paul23@gmail.com>
2023-10-17 18:58:11 -0700

    fic

--- _printf.c
+++ _printf.c
@@ -1,48 +1,36 @@
 #include "main.h"
+#include <stdarg.h>
 
-/**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+/** _printf - Entry poin
+ * @format: arg to be printed
+ *
+ * Return - number of char printed
  */
+
 int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
+	unsigned int i, j = 0 n = 0;
+	va_list arg;
+	va_start(arg, format);
 
-	va_list args;
-	int i = 0, j, len = 0;
-
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-Here:
-	while (format[i] != '\0')
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		j = 13;
-		while (j >= 0)
+		if (format[i] != '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
+			_putchar(format[i]);
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else if (format[i + 1] == 'c')
+		{
+			i++;
+			_putchar(va_arg(arg, int));
+		}
+		else if (format[i + 1] == 's')
+		{
+			n = _prints(va_arg(arg, char *s));
+			j = j + n;
+			j -= 1;
+		}
+		j++;
 	}
-	va_end(args);
-	return (len);
-
-
+	return (j);
 }
