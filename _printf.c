be7da2b90b0dfdd64c2a5dc764e90094c8a177db
Annonymous0395 <fatoki.paul23@gmail.com>
2023-10-17 18:40:31 -0700

    fic

--- _printf.c
+++ _printf.c
@@ -44,38 +44,5 @@ Here:
 	va_end(args);
 	return (len);
 
-#include <stdarg.h>
 
-/** _printf - Entry poin
- * @format: arg to be printed
- *
- * Return - number of char printed
- */
-
-int _printf(const char *format, ...)
-{
-	unsigned int i, j = 0 n = 0;
-	va_list arg;
-	va_start(arg, format);
-
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-		}
-		else if (format[i + 1] == 'c')
-		{
-			i++;
-			_putchar(va_arg(arg, int));
-		}
-		else if (format[i + 1] == 's')
-		{
-			n = _prints(va_arg(arg, char *s));
-			j = j + n;
-			j -= 1;	
-		}
-		j++;
-	}
-	return (j);
 }
